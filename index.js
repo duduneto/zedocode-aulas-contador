52bb7b5bca23f63ce23bbb547bca945dc21f9c6c
Dudu Neto <dudumatosneto@gmail.com>
2021-07-04 13:06:43 -0300

    Adding style and btn reset

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var valorDoContador = 150;
+var valorDoContador = 0;
 
 var contador = document.getElementById('display-contador');
 contador.innerHTML = valorDoContador;
 
 var botaoAdicionar = document.getElementById('button-adicionar');
 var botaoRemover = document.getElementById('button-remover');
+var botaoReset = document.getElementById('btn-reset');
 
 function adicionar() {
     valorDoContador = valorDoContador + 1;
@@ -14,7 +15,12 @@ function remover() {
     valorDoContador = valorDoContador - 1;
     contador.innerHTML = valorDoContador;
 }
+function resetar() {
+    valorDoContador = 0;
+    contador.innerHTML = valorDoContador;
+}
 
 botaoAdicionar.addEventListener('click', adicionar)
 botaoRemover.addEventListener('click', remover)
+botaoReset.addEventListener('click', resetar)
 
